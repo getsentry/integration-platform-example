ddac02c19756b289383720b6ed10ac39db3676f7
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-02-24 16:04:57 -0500

    feat(tooling): Setup typescript frontend starter code (#4)
    See [API-2438](https://getsentry.atlassian.net/browse/API-2438)
    
    This PR was split up from https://github.com/getsentry/integration-platform-example/pull/1.
    
    **frontend-ts**
    - TS compiles before building output, 
    - Has ESLint and Prettier
    - Has testing folder set up

---
+++ frontend-ts/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-console': 2,
+      },
+    },
+  ],
+};
---
+++ frontend-ts/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+// TS hints also work
+import {Example} from '../../data/types';
+
+function App() {
+  // Editor linting works
+  console.log('Testing');
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
