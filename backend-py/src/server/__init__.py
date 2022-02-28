3d90e107731be408c808f8a035a39c3197c79632
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-02-28 14:53:13 -0800

    💚 Check formatting in CI

--- backend-py/src/server/__init__.py
+++ backend-py/src/server/__init__.py
@@ -6,9 +6,9 @@ load_dotenv()
 server = Flask(__name__)
 
 
-def create_app(test_config={}):
+def create_app(config={}):
     server = Flask(__name__, instance_relative_config=True)
-    server.config.from_mapping(test_config)
+    server.config.from_mapping(config)
 
     @server.route("/")
     def hello_world():
