3c72ab3db712e53d195a5ae98d9ddbb8d541f9fa
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-02-28 18:12:21 -0800

    🔐 Simplify loading environment variables

--- backend-py/src/server.py
+++ backend-py/src/server.py
@@ -1,21 +1,15 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 server = Flask(__name__)
-
-# Identify the path to the root-level .env file
-env_path = os.path.join(os.path.dirname(__file__), "../../.env")
-
-# Iterate through the file and set all environment variables
-# with open(env_path, "r") as env:
-#     var_name, var_value = env.readline().split("=")
-#     os.environ[var_name] = var_value
 
 
 @server.route("/")
 def hello_world():
-    print("Getting here")
     return "<p>Hello World!</p>"
 
 
--- backend-ts/src/server.ts
+++ backend-ts/src/server.ts
@@ -5,7 +5,7 @@ import {Example} from './../../data/types';
 
 // Load .env environment file from the project root
 // We need to traverse up from /backend-ts/dist/server.js
-config({path: path.resolve(__dirname, '../../.env'), debug: true});
+config({path: path.resolve(__dirname, '../.env'), debug: true});
 
 const app = express();
 
