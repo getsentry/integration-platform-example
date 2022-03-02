f05ad319693ddeee5b225a2b686d346d7fc41fdb
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-03-02 13:31:29 -0800

    ✨ Setup uninstallation for backend-ts

---
+++ backend-ts/src/api/sentry/webhook.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import {VerifyResponseData} from '../setup';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const {action, data} = req.body;
+
+  // Handle Uninstallation webhook...
+  if (action === 'deleted' && !!data.installation) {
+    handleUninstall(data);
+  }
+
+  res.sendStatus(200);
+});
+
+function handleUninstall(data: VerifyResponseData) {
+  // In practice, this would be where you could remove the saved token/refreshToken
+  // from your DB, or delete other information as clean up.
+  console.info(`Uninstalled ${data.app.slug} from '${data.organization.slug}'`);
+}
+
+export default router;
