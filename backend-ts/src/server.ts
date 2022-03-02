e4c235e5029e9a3699bab8b9f94f2e69eb3055c6
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-03-02 15:53:16 -0800

    ✅ Fix failing test

--- backend-ts/src/server.ts
+++ backend-ts/src/server.ts
@@ -5,6 +5,8 @@ config({debug: true});
 
 const server = express();
 
+server.get('/', (req, res) => res.sendStatus(200));
+
 server.listen(process.env.BACKEND_PORT, () => {
   console.log(`Server started at http://localhost:${process.env.BACKEND_PORT}`);
 });
--- backend-ts/src/tests/server.spec.ts
+++ backend-ts/src/tests/server.spec.ts
@@ -3,10 +3,8 @@ import server from '../server';
 import request from 'supertest';
 
 describe('GET /', () => {
-  it('responds with text', async () => {
+  it('responds with a 200', async () => {
     const res = await request(server).get('/');
     assert.equal(res.statusCode, 200);
-    assert.deepEqual(res.body, {});
-    assert.typeOf(res.text, 'string');
   });
 });
