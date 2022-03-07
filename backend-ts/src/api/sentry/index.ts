83894890f178eda4aec2ca06ca2aeac30bd5fee8
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-03-07 13:50:00 -0800

    ✨ Verify the signature on requests

--- backend-ts/src/api/sentry/index.ts
+++ backend-ts/src/api/sentry/index.ts
@@ -1,15 +1,25 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import {createHmac} from 'crypto';
 import setupRoutes from './setup';
 import webhookRoutes from './webhook';
 
 const router = express.Router();
 
-router.use('/setup', setupRoutes);
-router.use('/webhook', webhookRoutes);
-router.get('/:installationId', (req, res) => {
-  const {installationId} = req.params;
-  // Make an API call with an associated token to test it's working
-  // Then manually expire the token in Sentry and test that the refresh is issued
-});
+// This function will authenticate that the requests are coming from Sentry.
+// Now, we can be confident in our nested routes that the data is legit, without having to repeat this check.
+// See more: https://docs.sentry.io/product/integrations/integration-platform/webhooks/
+function verifySentrySignature(req: Request, res: Response, next: NextFunction) {
+  const hmac = createHmac('sha256', process.env.SENTRY_CLIENT_SECRET);
+  hmac.update(JSON.stringify(req.body), 'utf8');
+  const digest = hmac.digest('hex');
+  if (digest === req.headers['sentry-hook-signature']) {
+    console.info('Verified: Request came from Sentry');
+    return next();
+  }
+  res.status(401).send({error: 'Could not verify request came from Sentry'});
+}
+
+router.use('/setup', verifySentrySignature, setupRoutes);
+router.use('/webhook', verifySentrySignature, webhookRoutes);
 
 export default router;
