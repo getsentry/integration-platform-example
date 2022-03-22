1464d9fa34e938c96830526c4a13fac8c909e61a
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-03-22 15:38:53 -0700

    Check resource type from header

--- backend-py/src/api/webhook.py
+++ backend-py/src/api/webhook.py
@@ -15,7 +15,9 @@ def webhook_index():
     action = request.json.get("action")
     data = request.json.get("data")
 
-    if action == "deleted":
+    resource = request.headers.get('sentry-hook-resource')
+
+    if resource == 'installation' and action == 'deleted':
         handle_uninstall(data["installation"])
 
     return '', 200
