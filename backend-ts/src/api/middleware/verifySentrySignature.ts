9d4e74bec1416bb3313519f6d666d25549f0e471
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-05-11 16:58:53 -0700

    💩 Write hacky verification for ts

--- backend-ts/src/api/middleware/verifySentrySignature.ts
+++ backend-ts/src/api/middleware/verifySentrySignature.ts
@@ -1,6 +1,11 @@
 import {createHmac} from 'crypto';
 import {NextFunction, Request, Response} from 'express';
 
+function getSignatureBody(req: Request): string {
+  const stringifiedBody = JSON.stringify(req.body);
+  return stringifiedBody === '{}' ? '' : stringifiedBody;
+}
+
 // This function will authenticate that the requests are coming from Sentry.
 // Now, we can be confident in our nested routes that the data is legit,
 // without having to repeat this check.
@@ -10,15 +15,17 @@ export default function verifySentrySignature(
   next: NextFunction
 ) {
   // TODO(Leander): Continue signature verification once partners have been notified of changes
-  return next();
   if (process.env.NODE_ENV == 'test') {
     return next();
   }
   const hmac = createHmac('sha256', process.env.SENTRY_CLIENT_SECRET);
 
-  hmac.update(JSON.stringify(request.body), 'utf8');
+  hmac.update(getSignatureBody(request), 'utf8');
   const digest = hmac.digest('hex');
-  if (digest === request.headers['sentry-hook-signature']) {
+  if (
+    digest === request.headers['sentry-hook-signature'] ||
+    digest === request.headers['sentry-app-signature']
+  ) {
     console.info('Authorized: Verified request came from Sentry');
     return next();
   }
