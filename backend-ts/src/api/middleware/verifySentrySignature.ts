d8ef3736ff5bd5e7f48afdf671c668c53dffc2e2
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-05-13 13:35:25 -0700

    chore(auth): Show necessary hack for current auth workflow (#39)
    This PR adds back the signature verification for all incoming requests. Unfortunately, it looks pretty hacky and fragile, but it is what some of our partners have to do if we don't migrate the scheme to a single header, and actual JSON payload.
    
    This PR should serve as an argument in favour of standardizing the headers and payloads on our results.
    
    For more info on how this occurs, and why it's not great:
    
    Sentry is sending a application/json header, with a empty body (b'' in python). In Flask, this will automatically respond with a 400 if you attempt request.json, and in Express, it automatically coerces the empty body into {} since that is valid JSON.
    
    Since we use the app secret and the request body to create the header, this leads to inconsistency with how developers are expected to parse the body, forcing them to always use the raw body since early stage middlewares or helpers (request.json, bodyParser.json()) might alter the payload.
    
    It's difficult to do that in JavaScript since the rest of the API consumes JSON requests, so I manually have to check for req.body === {} and convert it to '' . In Flask, I can't use request.json and have to request.data to access the raw byte string, but this is only because the header isn't telling the truth about the Content-Type.
    
    Additionally, due to a typo in Sentry code, we have two headers which may or may not have the signature to verify against.

--- backend-ts/src/api/middleware/verifySentrySignature.ts
+++ backend-ts/src/api/middleware/verifySentrySignature.ts
@@ -1,24 +1,38 @@
 import {createHmac} from 'crypto';
 import {NextFunction, Request, Response} from 'express';
 
-// This function will authenticate that the requests are coming from Sentry.
-// Now, we can be confident in our nested routes that the data is legit,
-// without having to repeat this check.
+// There are few hacks in this verification step (denoted with HACK) that we at Sentry hope
+// to migrate away from in the future. Presently however, for legacy reasons, they are
+// necessary to keep around, so we've shown how to deal with them here.
+
+function getSignatureBody(req: Request): string {
+  const stringifiedBody = JSON.stringify(req.body);
+  // HACK: This is necessary since express.json() converts the empty request body to {}
+  return stringifiedBody === '{}' ? '' : stringifiedBody;
+}
+
 export default function verifySentrySignature(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  // TODO(Leander): Continue signature verification once partners have been notified of changes
-  return next();
+  /**
+   * This function will authenticate that the requests are coming from Sentry.
+   * It allows us to be confident that all the code run after this middleware are
+   * using verified data sent directly from Sentry.
+   */
   if (process.env.NODE_ENV == 'test') {
     return next();
   }
   const hmac = createHmac('sha256', process.env.SENTRY_CLIENT_SECRET);
 
-  hmac.update(JSON.stringify(request.body), 'utf8');
+  hmac.update(getSignatureBody(request), 'utf8');
   const digest = hmac.digest('hex');
-  if (digest === request.headers['sentry-hook-signature']) {
+  if (
+    // HACK: The signature header may be one of these two values
+    digest === request.headers['sentry-hook-signature'] ||
+    digest === request.headers['sentry-app-signature']
+  ) {
     console.info('Authorized: Verified request came from Sentry');
     return next();
   }
