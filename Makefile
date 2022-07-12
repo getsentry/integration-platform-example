getsentry/integration-platform-example

commit 98d056dfffd92f6a0f1c840a5b82c0adf19502bd

ci(feat): Add Docker compose building workflow (#51)
This ensures that we can still build the Docker images.

There's also few minor changes.

.github/workflows/backend-py.yml
@@ -1,6 +1,10 @@
 name: backend-py
 on:
   pull_request:
+    paths:
+      - ".github/actions/setup-test"
+      - ".github/workflows/backend-py.yml"
+      - "backend-py/*"
   push:
     branches:
       - main
@@ -39,7 +43,7 @@ jobs:
         with:
           python-version: "3.9"
 
-      - uses: './.github/actions/setup-test'
+      - uses: "./.github/actions/setup-test"
 
       - name: Install Python Dependecies
         working-directory: backend-py

backend-py/Dockerfile
@@ -1,7 +1,11 @@
 FROM python:3.9 as backend-py
-# Installing packages...
+
 WORKDIR /backend-py
-COPY . .
+# Re-create the requirements layer if the requirements change
+COPY requirements.txt .
 RUN pip install -r requirements.txt
+
+# Copy the rest of the source code now, otherwise, code changes will invalidate the requirements cache
+COPY ./src/* /backend-py/
 # Preparing startup...
 CMD ["flask", "run"]

.github/workflows/backend-ts.yml
@@ -1,6 +1,10 @@
 name: backend-ts
 on:
   pull_request:
+    paths:
+      - ".github/actions/*"
+      - ".github/workflows/backend-ts.yml"
+      - "backend-ts/*"
   push:
     branches:
       - main
@@ -12,7 +16,7 @@ jobs:
 
     steps:
       - uses: actions/checkout@v2
-      - uses: './.github/actions/setup-node'
+      - uses: "./.github/actions/setup-node"
         with:
           project_directory: backend-ts
 
@@ -26,10 +30,10 @@ jobs:
 
     steps:
       - uses: actions/checkout@v2
-      - uses: './.github/actions/setup-node'
+      - uses: "./.github/actions/setup-node"
         with:
           project_directory: backend-ts
-      - uses: './.github/actions/setup-test'
+      - uses: "./.github/actions/setup-test"
 
       - name: Run Tests
         working-directory: backend-ts

.github/workflows/docker.yml
@@ -1,0 +1,28 @@
+name: docker
+on:
+  pull_request:
+    paths:
+      - ".github/workflows/docker.yml"
+      - ".dockerignore"
+      - ".env.sample"
+      - "docker-compose.yml"
+      - "backend-py/*"
+      - "backend-ts/*"
+      - "frontend/*"
+  push:
+    branches:
+      - main
+
+jobs:
+  docker:
+    name: Docker Compose builds
+    runs-on: ubuntu-latest
+    timeout-minutes: 10
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+
+      - name: Build Docker image
+        run: |
+          cp .env.sample .env
+          docker compose build

.github/workflows/frontend-ts.yml
@@ -1,6 +1,10 @@
 name: frontend
 on:
   pull_request:
+    paths:
+      - ".github/actions/*"
+      - ".github/workflows/frontend-ts.yml"
+      - "frontend/*"
   push:
     branches:
       - main
@@ -12,7 +16,7 @@ jobs:
 
     steps:
       - uses: actions/checkout@v2
-      - uses: './.github/actions/setup-node'
+      - uses: "./.github/actions/setup-node"
         with:
           project_directory: frontend
 
@@ -26,7 +30,7 @@ jobs:
 
     steps:
       - uses: actions/checkout@v2
-      - uses: './.github/actions/setup-node'
+      - uses: "./.github/actions/setup-node"
         with:
           project_directory: frontend
 

commit 8bf07f2981564e2eff35c6810d14f3b6025505fb

feat: Fail when image does not run
I introduced a regression in #51 because the backend-py code has relative import of the database code [1]

This also introduces a way to notice that a image has stopped starting up.

[1]
```text
backend-py  | Traceback (most recent call last):
backend-py  |   File "/usr/local/lib/python3.9/site-packages/flask/cli.py", line 260, in locate_app
backend-py  |     __import__(module_name)
backend-py  |   File "/backend-py/__init__.py", line 1, in <module>
backend-py  |     from .item import Item
backend-py  |   File "/backend-py/item.py", line 6, in <module>
backend-py  |     from .. import database
backend-py  | ImportError: attempted relative import beyond top-level package
```

.github/workflows/backend-py.yml
@@ -29,3 +29,10 @@ jobs:
       - name: Run Tests
         working-directory: backend-py
         run: pytest
+
+  docker:
+    timeout-minutes: 10
+    runs-on: ubuntu-20.04
+    steps:
+      - name: Test starts up
+        run: docker compose up backend-py --exit-code-from backend-py

Makefile
@@ -21,10 +21,10 @@ help:
 # Quickstart
 
 serve-python:
-	docker compose up frontend backend-py
+	docker compose up --build frontend backend-py
 
 serve-typescript:
-	docker compose up frontend backend-ts
+	docker compose up --build frontend backend-ts
 
 # Debugging
 

backend-py/Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9 as backend-py
+FROM python:3.9-slim as backend-py
 
 WORKDIR /backend-py
 # Re-create the requirements layer if the requirements change